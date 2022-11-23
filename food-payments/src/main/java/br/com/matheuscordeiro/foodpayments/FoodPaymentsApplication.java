38bb87f80a65dfdc418380f62d1e8023d15eb530
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-22 23:13:56 -0300

    First commit

---
+++ food-payments/src/main/java/br/com/matheuscordeiro/foodpayments/FoodPaymentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheuscordeiro.foodpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPaymentsApplication.class, args);
+	}
+
+}
---
+++ food-payments/src/test/java/br/com/matheuscordeiro/foodpayments/FoodPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuscordeiro.foodpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

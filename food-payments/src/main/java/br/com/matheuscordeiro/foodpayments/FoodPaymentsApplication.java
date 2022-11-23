e007eb34c8bceb73527cd487d59f3aa35f333b7e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-23 00:47:06 -0300

    Enable eureka client

--- food-payments/src/main/java/br/com/matheuscordeiro/foodpayments/FoodPaymentsApplication.java
+++ food-payments/src/main/java/br/com/matheuscordeiro/foodpayments/FoodPaymentsApplication.java
@@ -2,8 +2,10 @@ package br.com.matheuscordeiro.foodpayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FoodPaymentsApplication {
 
 	public static void main(String[] args) {

3116529888cdac5075dedca4c0218f0b4afd5165
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-23 00:51:17 -0300

    Orders

---
+++ food-orders/src/test/java/br/com/matheuscordeiro/foodorders/FoodOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheuscordeiro.foodorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

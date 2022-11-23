3c6481d723a35d405bffef4808e28250e1bac63d
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-23 00:23:04 -0300

    Model mapper config

---
+++ food-payments/src/main/java/br/com/matheuscordeiro/foodpayments/configs/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package br.com.matheuscordeiro.foodpayments.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}

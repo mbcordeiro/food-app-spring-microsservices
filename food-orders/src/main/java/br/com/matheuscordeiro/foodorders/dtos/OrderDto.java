2a413622c2e3bba86f61d4159b7ac07fd6cab22a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-23 23:34:13 -0300

    Order dto

---
+++ food-orders/src/main/java/br/com/matheuscordeiro/foodorders/dtos/OrderDto.java
@@ -0,0 +1,23 @@
+package br.com.matheuscordeiro.foodorders.dtos;
+
+import br.com.matheuscordeiro.foodorders.models.OrderItem;
+import br.com.matheuscordeiro.foodorders.models.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private Long id;
+    private LocalDateTime date;
+    private Status status;
+    private List<OrderItem> orderItems = new ArrayList<>();
+}

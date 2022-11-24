e9f36c0e2c1a7382c8623bf9ae87472799345738
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-24 00:35:08 -0300

    Eureka server orders and gateway project

--- food-orders/src/main/java/br/com/matheuscordeiro/foodorders/FoodOrdersApplication.java
+++ food-orders/src/main/java/br/com/matheuscordeiro/foodorders/FoodOrdersApplication.java
@@ -2,8 +2,10 @@ package br.com.matheuscordeiro.foodorders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FoodOrdersApplication {
 
 	public static void main(String[] args) {

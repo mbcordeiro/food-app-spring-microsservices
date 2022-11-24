7e3553211defb5c9f7ff8c0bd12f81a87a345ca2
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-11-23 23:21:53 -0300

    Order entity

---
+++ food-orders/src/main/java/br/com/matheuscordeiro/foodorders/Order.java
@@ -0,0 +1,29 @@
+package br.com.matheuscordeiro.foodorders;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    private LocalDateTime date;
+
+    @NotNull
+    private String status;
+}
